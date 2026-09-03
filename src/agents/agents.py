59cfe14a96fb0998b836e652ddc97dd42fe15c6f
Moumen <mmnnedjar@gmail.com>
2026-09-03 11:26:05 +0100

    adding gemini chat interface to use its llm

--- src/agents/agents.py
+++ src/agents/agents.py
@@ -1,16 +1,22 @@
 from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
+from langchain_xai import ChatXAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_google_genai import ChatGoogleGenerativeAI
 from src.tools.tools import web_search, scrape_url
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
 
 
 # model initialization
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=os.getenv("GEMINI_API_KEY"),
+)
 
 
 # first agent (search agent)
